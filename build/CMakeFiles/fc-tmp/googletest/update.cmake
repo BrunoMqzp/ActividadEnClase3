cb3f9d16b40367af5acc591533ce51a6a2ae15c8
BrunoMqzp <A00834415@tec.mx>
2025-01-29 20:05:45 -0600

    Change

--- build/CMakeFiles/fc-tmp/googletest/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing update step for googletest")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/VS/PROJECTS/SINTAXIS/GITHUB/ActividadEnClase3/build/CMakeFiles/fc-tmp/googletest/googletest-gitupdate.cmake")
-
-endblock()
